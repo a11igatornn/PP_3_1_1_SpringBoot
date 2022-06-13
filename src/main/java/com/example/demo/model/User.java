1e5775914451d5355dd061b7d9ef161b872cab6d
a11igatornn <cgs-nn@mail.ru>
2022-06-13 17:11:18 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table (name = "users")
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "last_name")
+    private String lastName;
+
+    @Column (name = "email")
+    private String email;
+}
